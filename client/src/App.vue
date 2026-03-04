bbfe20554af51ee2caffb5d9dcc0f11a56ae0829
Taumas <taumas.contact@gmail.com>
2026-03-04 19:35:26 +0100

    feat: user can parse pdf to text

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,16 +1,44 @@
 <script setup lang="ts">
+import { ref } from "vue";
+
+const file = ref<File | null>(null);
+
+const handleChangeFile = (event: Event) => {
+  const input = event.target as HTMLInputElement;
+  file.value = input.files?.[0] ?? null;
+};
+
 const sendRequest = async () => {
-  const response = await fetch("http://localhost:8000");
-  const data = await response.json();
-  console.log(data);
+  if (file.value) {
+    const formData = new FormData();
+    formData.append("book", file.value);
+
+    const response = await fetch("http://localhost:8000/upload", {
+      method: "POST",
+      body: formData,
+    });
+    const data = await response.json();
+    console.log(data);
+  } else {
+    console.error("Please enter a file");
+    //TODO: add a error message
+  }
 };
 </script>
 
 <template>
   <main>
     <h1>fablify</h1>
-    <button @click="sendRequest">send request</button>
+    <label>
+      Add your book as pdf <br />
+      <input @change="handleChangeFile" type="file" />
+    </label>
+    <button @click="sendRequest">Process file</button>
   </main>
 </template>
 
-<style></style>
+<style>
+label {
+  color: white;
+}
+</style>
--- server/main.py
+++ server/main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, HTTPException, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
+
+from utils import parsePDF
+
 
 app = FastAPI()
 
@@ -13,4 +16,17 @@ app.add_middleware(
 
 @app.get("/")
 def root():
-    return {"Hello": "World"}
+    return {"Message": "Please select a route"}
+
+
+@app.post("/upload")
+def upload_file(book: UploadFile = File(...)):
+    if book.content_type != "application/pdf":
+        raise HTTPException(status_code=400, detail="File must be a PDF")
+
+    # Convert pdf into text
+    result = parsePDF.parse_pdf(book)
+    if not result:
+        raise HTTPException(status_code=400, detail="Could not parse pdf")
+
+    return {"message": "File received", "result": result}
